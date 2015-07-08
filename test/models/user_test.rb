13f26fbac0dbc26644e1784616fd439c5bc6d207
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-08 12:15:02 -0600

    passing skipped test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,20 @@
 require_relative '../test_helper'
 
 class UserTest < ActiveSupport::TestCase
- # def test_verify_checks_user_role
- #   user = User.create(name: "Lev",
- #                      email: "lev@dev.com",
- #                      password: "password")
- #   user.roles.create(name: "nacho lover")
+  def test_verify_checks_user_role
+    user = User.create(name: "Lev",
+                       email: "lev@dev.com",
+                       password: "password")
 
- #   assert user.verify?("nacho lover")
- #   refute user.verify?("admin")
- # end
+    admin_role = Role.create(name: "admin")
+
+    store = Store.create(name: "pizza palace")
+    store1 = Store.create(name: "pizza hut")
+
+    user.user_roles.create(role: admin_role,
+                            store: store)
+
+    assert user.verify?("admin", store)
+    refute user.verify?("admin", store1)
+  end
 end
