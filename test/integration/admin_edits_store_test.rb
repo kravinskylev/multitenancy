69448bdc5168aef7b513fa2bb24a86802b809709
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-08 10:38:50 -0600

    admin still rejected

--- test/integration/admin_edits_store_test.rb
+++ test/integration/admin_edits_store_test.rb
@@ -8,7 +8,10 @@ class AdminEditsStoreTest < ActionDispatch::IntegrationTest
   end
 
   def test_non_admin_cant_edit_a_store
-    user = User.create(name: "Justin", email: "jdawg@gmail.com", password: "password")
+    user = User.create(name: "Justin",
+                       email: "jdawg@gmail.com",
+                       password: "password")
+
     store = Store.create(name: "Pizza Palace")
 
     visit login_path
@@ -19,6 +22,25 @@ class AdminEditsStoreTest < ActionDispatch::IntegrationTest
 
     visit edit_store_path(store)
     assert_equal root_path, current_path
+  end
+
+  def test_admin_can_edit_stores
+    user = User.create(name: "Justin",
+                       email: "jdawg@gmail.com",
+                       password: "password")
+
+    user.roles.create(name: "admin")
+
+    store = Store.create(name: "Pizza Palace")
+
+    visit login_path
+
+    fill_in "Email", with: "jdawg@gmail.com"
+    fill_in "Password", with: "password"
+    click_on "Enter Storedom"
+
+    visit edit_store_path(store)
+    assert_equal edit_store_path(store), current_path
   end
 
 end
